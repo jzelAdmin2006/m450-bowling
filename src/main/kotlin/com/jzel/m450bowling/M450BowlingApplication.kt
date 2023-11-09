c6729285c187a352631e440c15aa739538f89503
Johannes Zeller <johannes_zeller@sluz.ch>
2023-11-09 11:43:21 +0100

    initalize project

---
+++ src/main/kotlin/com/jzel/m450bowling/M450BowlingApplication.kt
@@ -0,0 +1,11 @@
+package com.jzel.m450bowling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class M450BowlingApplication
+
+fun main(args: Array<String>) {
+    runApplication<M450BowlingApplication>(*args)
+}
