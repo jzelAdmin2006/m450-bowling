6dc2192f1f5bd81d305485f8b38cac828c96edb5
Johannes Zeller <jo.zeller@protonmail.com>
2023-11-16 21:07:02 +0100

    throw number is only unique together with frame id (ordinary migration not necessary because not deployed yet)

--- src/main/resources/db/migration/V3__Create_throw_table.sql
+++ src/main/resources/db/migration/V3__Create_throw_table.sql
@@ -2,7 +2,8 @@ CREATE TABLE throw
 (
     id           INT PRIMARY KEY IDENTITY,
     frame_id     INT                                   NOT NULL,
-    throw_number INT                                   NOT NULL UNIQUE,
+    throw_number INT                                   NOT NULL,
     pins_hit     INT CHECK (pins_hit BETWEEN 0 AND 10) NOT NULL,
-    FOREIGN KEY (frame_id) REFERENCES frame (id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (frame_id) REFERENCES frame (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (frame_id, throw_number)
 );
