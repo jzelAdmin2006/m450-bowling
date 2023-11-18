955d01231d95977d2861170f7d503c83d4e77f25
Johannes Zeller <jo.zeller@protonmail.com>
2023-11-18 21:53:06 +0100

    improve conciseness

--- src/test/kotlin/com/jzel/m450bowling/server/TestHelper.kt
+++ src/test/kotlin/com/jzel/m450bowling/server/TestHelper.kt
@@ -3,43 +3,35 @@ package com.jzel.m450bowling.server
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultMatcher
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 class TestHelper(private val mvc: MockMvc) {
-    fun laneThrow(pins: UInt) = performThrow(pins.toInt().toString(), MockMvcResultMatchers.status().isOk)
+
+    fun laneThrow(pins: UInt) = performThrow(pins.toString(), MockMvcResultMatchers.status().isOk)
+    fun invalidThrow(pins: Int) = performThrow(pins.toString(), MockMvcResultMatchers.status().isBadRequest)
     fun invalidThrow(pins: String) = performThrow(pins, MockMvcResultMatchers.status().isBadRequest)
-    fun invalidThrow(pins: Int) = invalidThrow(pins.toString())
 
     private fun performThrow(pins: String, statusMatcher: ResultMatcher) =
-        mvc.perform(MockMvcRequestBuilders.post("/throw/$pins").accept(MediaType.APPLICATION_JSON))
-            .andExpect(statusMatcher)
-            .andReturn().response.contentAsString
+        perform(MockMvcRequestBuilders.post("/throw/$pins"), statusMatcher)
 
-    fun activeGame(): String =
-        get("/game/active")
+    fun activeGame() = get("/game/active")
+    fun getGames() = get("/game")
 
-    fun endGame(): String =
-        performEnding(MockMvcResultMatchers.status().isOk)
+    fun endGame() = endGameWithStatus(MockMvcResultMatchers.status().isOk)
+    fun endGameTooEarly() = endGameWithStatus(MockMvcResultMatchers.status().isBadRequest)
 
-    fun endGameTooEarly(): String =
-        performEnding(MockMvcResultMatchers.status().isBadRequest)
+    private fun endGameWithStatus(statusMatcher: ResultMatcher) =
+        perform(MockMvcRequestBuilders.put("/game"), statusMatcher)
 
-    private fun performEnding(result: ResultMatcher) =
-        mvc.perform(MockMvcRequestBuilders.put("/game").accept(MediaType.APPLICATION_JSON))
-            .andExpect(result)
-            .andReturn().response.contentAsString
-
-    fun getGames(): String =
-        get("/game")
+    fun reset() = perform(MockMvcRequestBuilders.delete("/game"), MockMvcResultMatchers.status().isOk)
 
     private fun get(endpoint: String) =
-        mvc.perform(MockMvcRequestBuilders.get(endpoint).accept(MediaType.APPLICATION_JSON))
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andReturn().response.contentAsString
+        perform(MockMvcRequestBuilders.get(endpoint), MockMvcResultMatchers.status().isOk)
 
-    fun reset(): String =
-        mvc.perform(MockMvcRequestBuilders.delete("/game").accept(MediaType.APPLICATION_JSON))
-            .andExpect(MockMvcResultMatchers.status().isOk)
+    private fun perform(requestBuilder: MockHttpServletRequestBuilder, statusMatcher: ResultMatcher) =
+        mvc.perform(requestBuilder.accept(MediaType.APPLICATION_JSON))
+            .andExpect(statusMatcher)
             .andReturn().response.contentAsString
 }
